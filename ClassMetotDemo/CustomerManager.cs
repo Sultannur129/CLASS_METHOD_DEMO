5c6bbc27406565e749390193b31b47add53e87ea
Sultannur <200201043@kocaeli.edu.tr>
2022-08-03 01:09:40 +0300

    Add project files.

---
+++ ClassMetotDemo/CustomerManager.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassMetotDemo
+{
+    internal class CustomerManager
+    {
+
+        List<Customer> list = new List<Customer>();
+        public void Set(Customer cm1)
+        {
+            
+
+            list.Add(cm1);
+        }
+        public void Remove(Customer cm1)
+        {
+            list.Remove(cm1);
+
+        }
+
+        public void Write()
+        {
+            foreach (var item in list)
+            {
+                Console.WriteLine(item.Id+" "+item.Name+" "+item.Surname+" "+item.age+" "+item.field);
+            }
+        }
+    }
+}
---
+++ ClassMetotDemo/Program.cs
@@ -0,0 +1,33 @@
+﻿namespace ClassMetotDemo
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Customer customer1 = new Customer();
+            Customer customer2 = new Customer();
+            customer1.Id = 1;
+            customer1.Name = "Ali";
+            customer1.Surname = "Bekçi";
+            customer1.age = 26;
+            customer1.field = "Mühendis";
+            customer2.Id = 2;
+            customer2.Name = "Veli";
+            customer2.Surname = "Bekçi";
+            customer2.age = 23;
+            customer2.field = "İşçi";
+            CustomerManager cma1=new CustomerManager();
+            cma1.Set(customer1);
+            cma1.Set(customer2);
+            
+            cma1.Write();
+            Console.WriteLine("Müşteri Eklendi.");
+            Console.WriteLine("------------------------------");
+            cma1.Remove(customer1);
+            cma1.Write();
+            Console.WriteLine("Müşteri Silindi.");
+            Console.WriteLine("------------------------------");
+
+        }
+    }
+}
